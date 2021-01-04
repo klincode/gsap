090b2808dda6540f34e6d6618ae317c6c78a5850
Adam <adam@klincode.pl>
2021-01-04 19:51:50 +0100

    react logo animation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useRef, useEffect } from "react";
+import gsap from "gsap";
 
 function App() {
+  let logoItem = useRef(null);
+  useEffect(() => {
+    console.log(logoItem);
+    gsap.from(logoItem, {
+      duration: 3,
+      opacity: 1,
+      y: -1000,
+      ease: "bounce",
+      rotate: 340,
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img
+          src={logo}
+          className='App-logo'
+          alt='logo'
+          ref={(el) => (logoItem = el)}
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
