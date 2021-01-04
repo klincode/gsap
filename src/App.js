730d6a7c1dd15eda2316e8448ea2275113467d14
Adam <adam@klincode.pl>
2021-01-04 20:04:00 +0100

    start https://www.youtube.com/watch?v=whk76VMOlp0&list=PLgcPxVODYXGIT9iX0uHBUYQSyqGvafwHT&index=2&ab_channel=WrongAkram

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import react, { useRef, useEffect } from "react";
+import gsap from "gsap";
 function App() {
+  let circle = useRef(null);
+  let circleRed = useRef(null);
+  let circleBlue = useRef(null);
+  useEffect(() => {
+    gsap.from(circle, {
+      duration: 0.8,
+      opacity: 0,
+      x: 40,
+      ease: "power3.inOut",
+    });
+
+    gsap.from(circleRed, {
+      duration: 0.8,
+      opacity: 0,
+      x: 40,
+      ease: "power3.inOut",
+      delay: 0.2,
+    });
+
+    gsap.from(circleBlue, {
+      duration: 0.8,
+      opacity: 0,
+      x: 40,
+      ease: "power3.inOut",
+      delay: 0.4,
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <div className='circle-container'>
+          <div className='circle' ref={(el) => (circle = el)}></div>
+          <div className='circle red' ref={(el) => (circleRed = el)}></div>
+          <div className='circle blue' ref={(el) => (circleBlue = el)}></div>
+        </div>
       </header>
     </div>
   );
