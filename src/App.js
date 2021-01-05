2d9a384aac09167a718c4162491b661b90a68ddd
Adam <adam@klincode.pl>
2021-01-05 18:53:33 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 import gsap from "gsap";
+import CSSRulePlugin from "gsap/CSSRulePlugin";
 import react, { useRef, useState, useEffect } from "react";
+import imagePhoto from "./images/banner.jpg";
+
 function App() {
+  let container = useRef(null);
+  let image = useRef(null);
+  let ImageReveal = CSSRulePlugin.getRule(".img-container:after");
+  // let imageReveal =
+
+  const tl = gsap.timeline();
+  useEffect(() => {
+    tl.to(container, { duration: 0, visibility: "visible" })
+      .to(ImageReveal, {
+        duration: 2,
+        width: "0%",
+        ease: "Power1.inOut",
+      })
+      .from(image, { duration: 1.4, scale: 1.6, ease: "Power", delay: -2 });
+  });
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='main'>
+      <div className='container' ref={(el) => (container = el)}>
+        <>
+          <div className='img-container'>
+            <img src={imagePhoto} alt='' ref={(el) => (image = el)} />
+          </div>
+        </>
+      </div>
+    </section>
   );
 }
 
