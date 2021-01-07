5ad79b21b395ff7fe32cbd5f699d59c126a7a38f
Adam <adam@klincode.pl>
2021-01-07 19:20:17 +0100

    works ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
 import logo from "./logo.svg";
 import "./App.css";
 import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import react, { useRef, useState, useEffect } from "react";
 function App() {
+  let a = useRef(null);
+  let b = useRef(null);
+  let c = useRef(null);
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.fromTo(
+      a.children,
+      { y: "+=100", opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.2,
+        duration: 1,
+        scrollTrigger: {
+          trigger: a, //elem,ent triggerujący
+          // start: "top top",
+          start: "top 40%", //gorna krawedz dotknie 40%od góry viewportu
+          //punkt startowy kiedy uruchamiac triggera
+          //  1top:elem,ent trigerujacy
+          // 2top:viewwport, jesli
+          //   gorna kraweddz elementu treiggerujacego dotknie górnej krawedzi viewportu to odpalaj
+          markers: true, //podpowiadaczka
+        },
+      }
+      // stager jesli mam wiecej elementow, czas miedzy animacjamit childrensów
+    );
+  }, []);
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+      <div class='hero'>
+        <h1>GSAP ScrollTrigger tutorial</h1>
+      </div>
+      <section class='a' ref={(el) => (a = el)}>
+        <h2>Get creative</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum nisi
+          repellendus explicabo distinctio cum nemo, iste excepturi? Libero,
+          mollitia suscipit corrupti impedit minus nemo consequatur modi
+          corporis, pariatur sit velit.
         </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      </section>
+      <section class='b' ref={(el) => (b = el)}>
+        <h2>Basic layout</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum nisi
+          repellendus explicabo distinctio cum nemo, iste excepturi? Libero,
+          mollitia suscipit corrupti impedit minus nemo consequatur modi
+          corporis, pariatur sit velit.
+        </p>
+      </section>
+      <section class='c' ref={(el) => (c = el)}>
+        <h2>Design patterns</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum nisi
+          repellendus explicabo distinctio cum nemo, iste excepturi? Libero,
+          mollitia suscipit corrupti impedit minus nemo consequatur modi
+          corporis, pariatur sit velit.
+        </p>
+      </section>
     </div>
   );
 }
